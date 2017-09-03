206329e480f33092094c10453752a4a78222b39e
Jeffrey Liu <scubajeffrey@gmail.com>
2017-09-03 23:09:49 +0800

    added Greater China SPN names

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
+# SPN names for Greater China
+PRODUCT_COPY_FILES += \
+	device/sample/etc/spn-conf.xml:system/etc/spn-conf.xml
+
 DEVICE_PACKAGE_OVERLAYS += \
     device/samsung/klimtlte/overlay
 
